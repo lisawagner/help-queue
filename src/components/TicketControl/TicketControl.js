68b60ecac9134663fc15df37ae61071ae74c039d
Lisa Wagner <lisawagner.online@gmail.com>
2022-08-08 10:07:05 -0700

    feat: test and add actions

--- src/components/TicketControl/TicketControl.js
+++ src/components/TicketControl/TicketControl.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux/es/exports'
 import PropTypes from 'prop-types'
+import * as a from '../../actions'
 // components
 import TicketList from './TicketList'
 import NewTicketForm from './NewTicketForm'
@@ -25,19 +26,14 @@ class TicketControl extends React.Component {
       });
     } else {
       const { dispatch } = this.props;
-      const action = {
-        type: 'TOGGLE_FORM'
-      }
+      const action = a.toggleForm()
       dispatch(action);
       }
   }
 
   handleDeletingTicket = (id) => {
     const { dispatch } = this.props;
-    const action = {
-      type: 'DELETE_TICKET',
-      id
-    }
+    const action = a.deleteTicket(id)
     dispatch(action);
     this.setState({selectedTicket: null});
   }
@@ -49,13 +45,7 @@ class TicketControl extends React.Component {
   handleEditingTicketInList = (ticketToEdit) => {
     const { dispatch } = this.props;
     const { id, names, location, issue } = ticketToEdit;
-    const action = {
-      type: 'ADD_TICKET',
-      id,
-      names,
-      location,
-      issue,
-    }
+    const action = a.addTicket(ticketToEdit)
     dispatch(action)
     this.setState({
       editing: false,
@@ -66,17 +56,9 @@ class TicketControl extends React.Component {
   handleAddingNewTicketToList = (newTicket) => {
     const { dispatch } = this.props
     const {id, names, location, issue } = newTicket
-    const action = {
-      type: 'ADD_TICKET',
-      id,
-      names,
-      location,
-      issue,
-    }
+    const action = a.addTicket(newTicket)
     dispatch(action)
-    const action2 = {
-      type: 'TOGGLE_FORM'
-    }
+    const action2 = a.toggleForm()
     dispatch(action2);
   }
 
